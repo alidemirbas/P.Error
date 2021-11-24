ed3538927870b23d0bdaa9d3e1d5d8df87754f5a
Ali Demirbaş <vcs.ds@outlook.com>
2021-11-24 12:27:13 +0300

    comment deleted

--- P.ErrorManagement.Mvc/ErrorController.cs
+++ P.ErrorManagement.Mvc/ErrorController.cs
@@ -52,14 +52,6 @@ namespace P.ErrorManagement.Mvc
 
     public static class HttpRequestBaseExtension
     {
-        /// <summary>
-        /// Determines whether the specified HTTP request is an AJAX request.
-        /// </summary>
-        /// 
-        /// <returns>
-        /// true if the specified HTTP request is an AJAX request; otherwise, false.
-        /// </returns>
-        /// <param name="request">The HTTP request.</param><exception cref="T:System.ArgumentNullException">The <paramref name="request"/> parameter is null (Nothing in Visual Basic).</exception>
         public static bool IsAjaxRequest(this HttpRequest request)
         {
             if (request == null)
